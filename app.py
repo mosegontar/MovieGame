8d2f4236cdfa5509e9971fc516bca7cd53173c8f
Alex <mosegontar@gmail.com>
2016-03-11 13:10:02 -0500

    Change game.name and game.name_dict to game.current and game.current_list

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ from tomatopicker import Retriever
 import genres
 from flask import Flask, g, render_template, session, url_for, request, redirect
 
-app = Flask(__name__)
+app = Flask(__current__)
 
 
 # Config
 SECRET_KEY = '\xac\xd6z\xb3\xe4j&}\x120\xc71/{\xe4\x95\xa6\xdd_\x9e\x9e\xb1\xd3p'
-app.config.from_object(__name__)
+app.config.from_object(__current__)
 
 @app.before_request
 def before_request():
@@ -25,31 +25,31 @@ def before_request():
         g.game = Retriever()
 
         # if a game is not already in progress, set session variables to game object's initial values
-        if 'name' not in session.keys() or 'reset' in session.keys():
+        if 'current' not in session.keys() or 'reset' in session.keys():
             
             if 'reset' in session.keys():
                 del session['reset']                 
 
             g.game.start_at_top(session['starting_genres'])
             
+            self['connections'] = g.game.connections
             session['chain'] = g.game.chain
-            session['name'] = g.game.name
+            session['current'] = g.game.current
             
-            g.game.chain.append(g.game.name)
+            g.game.chain.append(g.game.current)
             
             session['strikes'] = g.game.strikes
             session['score'] = len(session['chain']) - 1
-            session['name_list'] = g.game.name_dict
-
+            session['current_list'] = g.game.current_list
 
         # if a game IS in progress, update the game object to the values stored in the session variables
         else:
 
             g.game.chain = session['chain']
-            g.game.name = session['name']
+            g.game.current = session['current']
             g.game.strikes = session['strikes']
             g.game.score = session['score']
-            g.game.name_dict = session['name_list']
+            g.game.current_list = session['current_list']
 
 
 @app.route('/reset', methods=['POST'])
@@ -79,6 +79,7 @@ def start():
         session.clear()
         return render_template('start.html', all_genres=genres.genres)
 
+
 @app.route('/play', methods=['GET', 'POST'])
 def game():
     """Runs game play"""
@@ -88,39 +89,41 @@ def game():
         
         choice = request.form['answer'].strip()
         
-        if choice.lower() in g.game.name_dict.keys():
+        if choice.lower() in g.game.current_list.keys():
         
-            g.game.name = choice.title()
+            g.game.current = choice.title()
         
-            link = g.game.name_dict.get(choice.lower())
+            link = g.game.current_list.get(choice.lower())
             link = g.game.fix_link(link)
         
-            g.game.chain.append(g.game.name)
+            g.game.chain.append(g.game.current)
+
+
             g.game.score += 1
         
             if len(g.game.chain) % 2 == 0:
-                g.game.name_dict = g.game.get_films(link)
+                g.game.current_list = g.game.get_films(link)
             else:
-                g.game.name_dict = g.game.get_cast(link)
+                g.game.current_list = g.game.get_cast(link)
         else:
             g.game.strikes += 1
 
         # Now update the session variables with the lastest game state 
         session['chain'] = g.game.chain
-        session['name'] = g.game.name
+        session['current'] = g.game.current
         session['strikes'] = g.game.strikes
         session['score'] = len(session['chain']) - 1
-        session['name_list'] = g.game.name_dict
+        session['current_list'] = g.game.current_list
 
 
     # If game in progress, render game with latest state
     if session['strikes'] < 3:
-        return render_template('game_play.html', current=session['name'], chain=session['chain'][::-1], score=session['score'], strikes=session['strikes'])
+        return render_template('game_play.html', current=session['current'], chain=session['chain'][::-1], score=session['score'], strikes=session['strikes'])
 
     # If game over (3 strikes), provide feedback and set session['gameover'] to True
     else:
         session['gameover'] = True
-        current = session['name']
+        current = session['current']
         chain = session['chain'][::-1]
         score = session['score']
         strikes = session['strikes']
@@ -129,5 +132,5 @@ def game():
 
 
 
-if __name__ == '__main__':
+if __current__ == '__main__':
     app.run(debug=True)
