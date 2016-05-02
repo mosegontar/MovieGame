a8b7e1c78733194d5ce9c3ac598e9fb875f55974
Alex Gontar <mosegontar@gmail.com>
2016-05-01 21:27:05 -0400

    Make changes to prepare for heroku deployment

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
 DEBUG = False
 SECRET_KEY = os.environ.get('SECRETKEY')
+API_KEY = os.environ.get("APIKEY")
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', default="sqlite:///")
