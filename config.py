c099b9177d5da71fa93e6ae945c23a1117629b07
Alex Gontar <mosegontar@gmail.com>
2016-05-01 21:32:41 -0400

    update config file

--- config.py
+++ config.py
@@ -2,4 +2,6 @@ import os
 DEBUG = False
 SECRET_KEY = os.environ.get('SECRETKEY')
 API_KEY = os.environ.get("APIKEY")
+SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', default="sqlite:///")
+
