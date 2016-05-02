0d01a8a05c7886424499bd1ac3da25db4d7a77a2
Alex Gontar <mosegontar@gmail.com>
2016-05-01 21:55:13 -0400

    Update config

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ DEBUG = False
 SECRET_KEY = os.environ.get('SECRETKEY')
 API_KEY = os.environ.get("APIKEY")
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', default="sqlite:///")
+SQLALCHEMY_DATABASE_URI = os.environ.get('HEROKU_POSTGRESQL_PINK_URL', default="sqlite:///")
 
